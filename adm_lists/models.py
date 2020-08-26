17e6a9b2443bc3638709e1cd334ae59805b1ef12
dany <danyzolotarev@gmail.com>
2020-08-26 22:22:02 +0300

    Models reconstruction

--- adm_lists/models.py
+++ adm_lists/models.py
@@ -10,19 +10,13 @@ from django.utils import timezone
 from datetime import datetime
 
 
-class Exam(models.Model):
-    subject = models.CharField(max_length=50, default='')
-
-
-class Score(models.Model):
-    subject = models.ForeignKey(Exam, default=0, on_delete=models.CASCADE)
-    result = models.IntegerField(default=0)
+class Subject(models.Model):
+    name = models.CharField(max_length=50, default='')
 
 
 class Student(models.Model):
     name = models.CharField(max_length=50, default='')
     birthday = models.DateTimeField(default=timezone.now())
-    scores = models.ManyToManyField(Score)
 
     def __str__(self):
         return self.name
@@ -31,10 +25,38 @@ class Student(models.Model):
         ordering = ['name']
 
 
-class Direction(models.Model):
-    d_id = models.CharField(max_length=50, default='')
+class Score(models.Model):
+    subject = models.ForeignKey(Subject, default=0, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    result = models.IntegerField(default=0)
+
+
+class University(models.Model):
     title = models.CharField(max_length=50, default='')
+
+
+class Faculty(models.Model):
+    faculty_id = models.CharField(max_length=50, default='')
+    title = models.CharField(max_length=50, default='')
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+
+
+class Direction(models.Model):
+    direction_id = models.CharField(max_length=50, default='')
+    title = models.CharField(max_length=50, default='')
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+
+class Program(models.Model):
+    program_id = models.CharField(max_length=50, default='')
+    title = models.CharField(max_length=50, default='')
+    direction = models.ForeignKey(Direction, on_delete=models.CASCADE)
     students = models.ManyToManyField(Student)
+
+    places = models.IntegerField()  # Кол-во мест
+    spec_places = models.IntegerField()  # Места по особой квоте
+    target_places = models.IntegerField()  # Целевые места
+    appl_count = models.IntegerField()  # Кол-во поданных заявлений
 
     def __str__(self):
         return self.title
@@ -46,36 +68,38 @@ class Direction(models.Model):
 class Admission(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
-    university = models.CharField(max_length=50)
-    level = models.CharField(max_length=50)
-    faculty = models.CharField(max_length=50)
+    # CHARACTERISTICS
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    faculty = models.ForeignKey(Faculty, models.CASCADE)
     direction = models.ForeignKey(Direction, models.CASCADE)
-    program = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
-    priority = models.CharField(max_length=50)
-    agreement = models.IntegerField()
-    comment = models.CharField(max_length=50)
+    program = models.ForeignKey(Program, models.CASCADE)
+    level = models.CharField(max_length=50)  # Бакалавриат etс
+    type = models.CharField(max_length=50)  # Очный / заочный
+    contract = models.CharField(max_length=50)  # Платка / Бюджет
+    contract_type = models.CharField(max_length=50)  # БВИ, договор, вне конкурса etc
 
+    # PERSONAL
+    reg_num = models.CharField(max_length=50)  # Рег. номер
+    priority = models.CharField(max_length=50)  # Приоритет
+    agreement = models.IntegerField()  # Наличие согласия
+
+    # EXAM POINTS
     total = models.IntegerField()
     exam_sum = models.IntegerField()
     exam_1 = models.IntegerField()
     exam_2 = models.IntegerField()
     exam_3 = models.IntegerField()
     bonus_sum = models.IntegerField()
-    bonus_str = models.CharField(max_length=50)
+    bonus_str = models.CharField(max_length=50)  # Олимпиады
+    comment = models.CharField(max_length=50)  # Комментарий (олимпиада БВИ etc)
 
+    # RATING
+    row_id = models.IntegerField()  # Место в списке
+
+    # TECHNICAL
     scrape_date = models.DateTimeField()
     url = models.CharField(max_length=50)
     updated = models.DateTimeField()
-
-    contract = models.CharField(max_length=50)
-    places = models.IntegerField()
-    spec_places = models.IntegerField()
-    target_places = models.IntegerField()
-    appl_count = models.IntegerField()
-    row_id = models.IntegerField()
-    reg_num = models.CharField(max_length=50)
-    contract_type = models.CharField(max_length=50)
 
     class Meta:
         managed = False
